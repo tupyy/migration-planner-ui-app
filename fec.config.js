9d9307b39b9cdde3f2f76cca4d8ead181f109e7e
Montse Ortega <mortegag@redhat.com>
2025-04-01 13:41:09 +0200

    Solving problems with app name

--- fec.config.js
+++ fec.config.js
@@ -4,14 +4,7 @@ module.exports = {
   appUrl: '/openshift/migration-assessment',
   debug: true,
   useProxy: true,
-  proxyVerbose: true,
-  /**
-   * Change accordingly to your appname in package.json.
-   * The `sassPrefix` attribute is only required if your `appname` includes the dash `-` characters.
-   * If the dash character is present, you will have add a camelCase version of it to the sassPrefix.
-   * If it does not contain the dash character, remove this configuration.
-   */
-  sassPrefix: '.assisted-migration-app, .assistedMigrationApp',
+  proxyVerbose: true,  
   /**
    * Change to false after your app is registered in configuration files
    */
