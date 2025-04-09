d44c4d78474e951dda3498203516377290443f70
Montse Ortega <mortegag@redhat.com>
2025-04-09 07:47:46 +0200

    Problems with token + breadcrumb

--- src/pages/MigrationWizardPage.tsx
+++ src/pages/MigrationWizardPage.tsx
@@ -7,7 +7,7 @@ const MigrationWizardPage: React.FC = () => {
   return (
     <AppPage
       breadcrumbs={[
-        { key: 1, to: "/migrate", children: "Migration assessment" },
+        { key: 1, to: "/openshift/migration-assessment", children: "Migration assessment" },
         { key: 2, to: "#", children: "Guide", isActive: true },
       ]}
       title="Welcome, let's start your migration journey from VMware to OpenShift."
